8641231f7cd7309c3c370269f2580c20fe3f6d9d
Petori <M201770455@hust.edu.cn>
2018-06-12 15:01:34 +0800

    修改simple_move.py以实时录/joint_states数据

--- ur_modern_driver/simple_move.py
+++ ur_modern_driver/simple_move.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
-# Created by Petori in 2018/6/7
-# I got bad velocity data when simulating this script by gazebo.
+# Simplified by Petori in 2018/3/31
 import time
 import roslib; roslib.load_manifest('ur_modern_driver')
 import rospy
 import actionlib
+import sys  
+import subprocess  
+import os
 from control_msgs.msg import *
 from trajectory_msgs.msg import *
 from sensor_msgs.msg import JointState
@@ -12,10 +14,9 @@ from math import pi
 
 JOINT_NAMES = ['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint',
                'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint']
-Q1 = [0,-1.57,0,-1.57,0,0]
-Q2 = [1.0,-0.57,-2.0,-0.57,1.0,1.0]
-Q3 = [-1.0,-2.57,2.0,-2.57,-1.0,-1.0]
-Q4 = [0,-1.57,0,-1.57,0,0]
+Q1 = [2.2,0,-1.57,0,0,0]
+Q2 = [1.5,0,-1.57,0,0,0]
+Q3 = [1.5,-0.2,-1.57,0,0,0]
     
 client = None
 
@@ -29,10 +30,9 @@ def move():
         joints_pos = joint_states.position
         g.trajectory.points = [
             JointTrajectoryPoint(positions=joints_pos, velocities=[0]*6, time_from_start=rospy.Duration(0.0)),
-            JointTrajectoryPoint(positions=Q1, velocities=[0.2,0.2,-0.4,0.2,0.2,0.2], time_from_start=rospy.Duration(1)),
-            JointTrajectoryPoint(positions=Q2, velocities=[0,0,0,0,0,0], time_from_start=rospy.Duration(3)),
-            JointTrajectoryPoint(positions=Q3, velocities=[0,0,0,0,0,0], time_from_start=rospy.Duration(6)),
-            JointTrajectoryPoint(positions=Q4, velocities=[0,0,0,0,0,0], time_from_start=rospy.Duration(8))]
+            JointTrajectoryPoint(positions=Q1, velocities=[0]*6, time_from_start=rospy.Duration(2.0)),
+            JointTrajectoryPoint(positions=Q2, velocities=[0]*6, time_from_start=rospy.Duration(3.0)),
+            JointTrajectoryPoint(positions=Q3, velocities=[0]*6, time_from_start=rospy.Duration(4.0))]
         client.send_goal(g)
         client.wait_for_result()
     except KeyboardInterrupt:
@@ -50,8 +50,18 @@ def main():
         print "Waiting for server..."
         client.wait_for_server()
         print "Connected to server"
+        child1 = subprocess.Popen('rostopic echo /joint_states >1.txt',shell=True)
         move()
         print "Trajectory finished"
+        time.sleep(0.5)
+        child2 = subprocess.Popen('cp 1.txt data/1.txt',shell=True)
+        time.sleep(0.5)
+        child3 = subprocess.Popen('rm 1.txt',shell=True)
+        time.sleep(0.5)
+        if True:
+            child1.kill()
+            child2.kill()
+            child3.kill()
     except KeyboardInterrupt:
         rospy.signal_shutdown("KeyboardInterrupt")
         raise
